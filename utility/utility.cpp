6ea1787c7f99d5a720581c20c2bfd0906a9cc883
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-02-03 11:42:40 +0000

    Comment typo change

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,4 +1,4 @@
 // This file does sweet fuck all, but the project doesn't compile
 // to a static library without it
 
-// Up yours Visual C++ compiler! I smarter than you.
+// Up yours, Visual C++ compiler! Me clever programmer. Me smarter than you.
