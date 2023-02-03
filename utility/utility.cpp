b23a4948448d4901d0a40818c7f315b7ad1e15c2
tjfacos <57414346+tjfacos@users.noreply.github.com>
2023-02-03 11:47:32 +0000

    Added comment

--- utility/utility.cpp
+++ utility/utility.cpp
@@ -1,4 +1,6 @@
 // This file does sweet fuck all, but the project doesn't compile
-// to a static library without it
+// to a static library without it. 
+
+// The header file(s) actually have useful stuff in them
 
 // Up yours, Visual C++ compiler! Me clever programmer. Me smarter than you.
